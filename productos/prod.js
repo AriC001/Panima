ee987d0a5563f4bb84dfaa366c27eebba38f42be
Ari Rafael Cohen <aricohen98@gmail.com>
2023-03-28 13:16:38 -0300

    Productos gap

--- productos/prod.js
+++ productos/prod.js
@@ -18,7 +18,7 @@ const productosJson = {
             "img": "./img/img_prod/FASTak.webp",
             "desc": "La familia de anclas con sutura enroscables FASTak/Bio-FASTak (2,4 mm y 2,8 mm de titanio, 3,0 mm de PLDLA) se utilizan para asegurar el labrum y el tejido blando asociado a la cavidad glenoidea por medio de la sutura FiberWire precargada. Una vez que las anclas son implantadas en la cavidad glenoidea (preperforación y terraja para la sutura Bio-FASTak), las suturas se pueden pasar en configuraciones múltiples para una fijación y resistencia óptimas usando el dispositivo para pasar suturas Labral Scorpion, QuickPass Suture Lasso u otro.La familia de anclas con sutura FASTak/Bio-FASTak (2,4 mm y 2,8 mm de titanio, 3,0 mm de PLDLA) fijan el labrum al tejido blando por medio del FiberWire precargado. Una vez que las anclas son implantadas (pre-perforación y terraja para la sutura Bio-FASTak), pueden tener múltiples configuraciones para una fijación y resistencia óptimas usando el dispositivo Labral Scorpion, QuickPass Suture Lasso u otro.",
             "descMob": "https://www.arthrex.com/es/hombro/fastak",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "ANCLAS CON SUTURA SIN NUDO SwiveLock®️ para labrum",
@@ -28,7 +28,7 @@ const productosJson = {
             "img": "./img/img_prod/SwiveLock.webp",
             "desc": "El SwiveLock para labrum sin nudos, atornillado se ha diseñado para usarse en el tratamiento de la inestabilidad del hombro. El ancla se utiliza como PushLock para el labrum. Ofrece todas las mismas ventajas sin nudos (bajo perfil, rápido, técnica de primera sutura con múltiples opciones de configuración de puntos) con los beneficios agregados de mayor resistencia a la extracción y conductos de comunicación que pueden permitir el crecimiento óseo interno. El ancla está disponible en materiales BioComposite o PEEK.",
             "descMob": "https://www.arthrex.com/es/hombro/swivelock-para-labrum",
-            "categoria": "Neuro"
+            "categoria": "Trauma"
         },
         {
             "titulo": "ANCLAS CON SUTURA PushLock®️",
@@ -38,7 +38,7 @@ const productosJson = {
             "img": "./img/img_prod/PushLock.webp",
             "desc": "El ancla PushLock es un ancla con sutura sin nudos diseñada para la reparación artroscópica de la inestabilidad o del manguito rotador. Con el diseño singular de las anclas PushLock, el cirujano puede ajustar la cantidad de tensión en el tejido durante la operación, lo que, a su vez, permite la reducción y fijación precisas del tejido. Está disponible en tamaños de 2,9, 3,5 y 4,5 mm. El ancla PushLock de 2,9 mm está optimizado para la reparación del labrum. El ancla PushLock de 4,5 mm se usa para la reparación del manguito rotador como parte de la técnica SutureBridge. El de 3,5 se puede usar para los procedimientos del manguito rotador o de la inestabilidad. El cuerpo del ancla está disponible en material BioComposite o PLLA bioabsorbibles y en PEEK no absorbible. Todos son implantes sólidos, que permiten realizar cambios y transparentes a las ondas radioeléctricas, sin falsas imágenes en RNM. Cada versión utiliza un ojal PEEK que le da mayor resistencia durante la inserción, lo que permite tensionar y hacer cambios al tejido con firmeza.",
             "descMob": "https://www.arthrex.com/es/hombro/pushlock",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "Cánula Twist-In™️",
@@ -48,7 +48,7 @@ const productosJson = {
             "img": "./img/img_prod/Twist-In.webp",
             "desc": "La cánula transparente Twist-In permite la visualización artroscópica directa de los instrumentos y el paso de las suturas a través de la cánula. El atado de nudos artroscópicos se simplifica dado que se pueden observar y corregir las suturas torcidas y enredadas durante el atado de nudos artroscópicos. La opción flexible se adapta fácilmente a instrumentos grandes o curvos. Cada cánula viene con una tapa antiderrame y obturador descartable. El diseño roscado inhibe en gran medida la caída de la cánula durante la remoción del instrumento y permite la retracción de la cápsula para brindar visualización. El canal lateral se puede usar para entrada de irrigación o salida pasiva.",
             "descMob": "https://www.arthrex.com/es/hombro/canula-twistin",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "FIJACION DE INJERTOS Tornillos de Biointerferencia",
@@ -58,7 +58,7 @@ const productosJson = {
             "img": "./img/img_prod/Biointerferencia.webp",
             "desc": "Los tornillos de Biointerferencia tienen una larga y exitosa historia clínica en la fijación de injertos HTH y de tejido blando. Estos tornillos se hacen de PLLA amorfo que permite una degradación controlada en el tiempo. Estos tornillos están disponibles de muchas longitudes, diámetros y diseños para diversas indicaciones y técnicas. La instrumentación simple asegura una inserción reproducible con un daño mínimo en el injerto de tejido y una resistencia de fijación máxima.",
             "descMob": "https://www.arthrex.com/es/rodilla/tornillos-de-biointerferencia",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "FIJACION DE INJERTOS Tornillos de interferencia BioComposite",
@@ -68,7 +68,7 @@ const productosJson = {
             "img": "./img/img_prod/BioComposite.webp",
             "desc": "Los tornillos de interferencia de BioComposite están compuestos de 30% fosfato de calcio bifásico y 70% PLDLA y han sido diseñados para usarse como dispositivo de fijación para injertos de hueso-tendón (HTH o Aquiles) y de tejido blando durante los procedimientos de reconstrucción del LCA y del LCP.Cada tornillo tiene un diseño cónico que maximiza el torque de inserción, al colocar el tornillo hasta el tope del destornillador. La forma de la rosca se ha optimizado para facilitar la inserción y maximizar la fijación de hueso y de tejido blando en hueso cortical y esponjoso.El destornillador hexalobular canulado mejora la familia de tornillos ya que proporciona un sistema de inserción universal para todos los tornillos con una mejora considerable de las fuerzas de torsión y de inserción. Cada tornillo encaja completamente y está totalmente sostenido en toda la longitud del extremo del insertador. De acuerdo con los informes clínicos, el fosfato de calcio bifásico es seguro y tiene un excelente potencial para las aplicaciones ortopédicas. Como tema central de muchos estudios de reemplazo óseo, la formación ósea precoz puede relacionarse con las propiedades biorreabsorbibles y osteoconductivas favorables de los fosfatos de calcio bifásico.",
             "descMob": "https://www.arthrex.com/es/rodilla/tornillos-de-interferencia-biocomposite",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "ACL TightRope®️ II Implant",
@@ -78,7 +78,7 @@ const productosJson = {
             "img": "./img/img_prod/acl-tightrope-ii-implant.webp",
             "desc": "As the first adjustable-loop cortical suspensory fixation implant to use a flat SutureTape design, the new ACL TightRope®️ II implant offers better handling characteristics and is more resistant to graft abrasion or tissue pull-through than traditional round sutures.1 Engineered for precise graft tensioning, the adjustable-loop mechanism allows for incremental retensioning of the graft construct after the implants have been secured on the cortex. The redesigned cortical button now incorporates a proprietary knotless fifth locking mechanism, increasing strength and resistance to cyclic displacement.2 To accommodate various graft types and techniques, TightRope II implants are available in RT and BTB configurations loaded with an additional flipping suture or preloaded with a FiberTape®️ suture for the InternalBrace™️ technique. Available options for the ABS implant include standard or open.",
             "descMob": "https://www.arthrex.com/knee/acl-tightrope-ii-implant",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "ACL TightRope®️",
@@ -88,7 +88,7 @@ const productosJson = {
             "img": "./img/img_prod/TightRope.webp",
             "desc": "ACL TightRope permite una fijación cortical regulable para la reconstrucción del ligamento cruzado. La fijación patentada de cuatro puntos sin nudos de Arthrex resiste el desplazamiento cíclico y proporciona alta fuerza de arranque. ACL TightRope hace innecesarios los implantes de varios tamaños y facilita el llenado completo con injerto de sockets femorales cortos, frecuentes en las perforaciones anatómicas del LCA.",
             "descMob": "https://www.arthrex.com/es/rodilla/acl-tightrope",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "Implante Bio-TransFix®️",
@@ -98,7 +98,7 @@ const productosJson = {
             "img": "./img/img_prod/Bio-TransFix.webp",
             "desc": "El implante Bio-TransFix de 5 mm, en longitudes de 40, 50 ó 60 mm, provee la máxima resistencia a la extracción y a la vez conserva métodos de implantación rápidos y eficientes. El material PLLA amorfo es transparente y se absorbe en un plazo de 3 a 5 años. Una mayor compresión del injerto en el túnel femoral brinda la fijación femoral transversa más segura de todas. El implante se golpea hasta fijar el injerto en su lugar en forma segura. La fijación con un pin Bio-TransFix cruzado brinda la fijación femoral más fuerte que cualquier implante bioabsorbible disponible para injertos de tejido blando o de hueso-tendón-hueso. Con una fijación simple con pin cruzado para tejidos blandos, la longitud y la carga a todas las fibras del injerto se igualan independientes de la fijación tibial, lo que potencia al máximo la rigidez del injerto. Para injertos de hueso-tendón-hueso, se usa la misma perforación de orificio único de 3 mm en la clavija del hueso para transportar el injerto y también para fijarlo en su lugar, lo que deja el resto del hueso intacto.",
             "descMob": "https://www.arthrex.com/es/rodilla/implante-biotransfix",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "TransFix de BioComposite®️",
@@ -108,7 +108,7 @@ const productosJson = {
             "img": "./img/img_prod/TransFix de BioComposite.webp",
             "desc": "TransFix de BioComposite ofrece una fuerte y reproducible fijación de los injertos de tejido blando al tiempo que permite la degradación osteoconductiva controlada. TransFix de BioComposite está compuesto de 30% fosfato de calcio bifásico y 70% PLDLA.",
             "descMob": "https://www.arthrex.com/es/rodilla/transfix-de-biocomposite",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "Meniscal Cinch™️",
@@ -118,7 +118,7 @@ const productosJson = {
             "img": "./img/img_prod/Meniscal Cinch.webp",
             "desc": "El implante Meniscal Cinch permite a los cirujanos reparar desgarros meniscales mediante la técnica artroscópica All-Inside, evitando las incisiones accesorias que se requieren en las técnicas tradicionales dentro/fuera que a menudo producen morbilidad adicional. Los implantes de PEEK de bajo perfil están cargados con una sutura FiberWire 2-0 preatada que se desliza fácilmente y permite un adecuado tensionamiento. El mango ergonómico y la firme cánula permiten una fácil inserción en la articulación y un posicionamiento preciso sobre el menisco. El tope de profundidad externo asegura que el implante sea colocado dentro de la cápsula, protegiendo las estructuras posteriores de la rodilla. El extremo de la sutura 2-0 es extraarticular y puede tensionarse para evitar que la sutura se afloje en la articulación y lograr mejor visualización. El empujador de nudos/cortador de suturas permite fácilmente que el nudo deslizante quede ligeramente por debajo del menisco y remueve el sobrante de sutura en un paso.",
             "descMob": "https://www.arthrex.com/es/rodilla/meniscal-cinch",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "Meniscal Cinch™️ II Device",
@@ -128,7 +128,7 @@ const productosJson = {
             "img": "./img/img_prod/Meniscal Cinch II Device.webp",
             "desc": "El implante Meniscal Cinch II ofrece un mecanismo de colocación lineal para proporcionar un manejo ergonómico y simple. La cánula plástica de bajo perfil además de ayudar a proteger el cartílago articular en el momento de acceder dentro de la articulación, nos proporciona mayor flexibilidad y mejor acceso en rodillas con espacios articulares reducidos facilitando la colocación activa del implante. Los implantes de PEEK de 1 mm × 5 mm están interconectados con una sutura sin núcleo FiberWire®️ 2-0, con un nudo deslizante Weston modificado, pre-anudado que se tensiona fácilmente, permitiendo realizar reparaciones verticales u horizontales con suturas seguras y de bajo perfil.",
             "descMob": "https://www.arthrex.com/es/rodilla/meniscal-cinch-ii-device",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "FIJACION OSTEOCONDREAL OATS®️ para autoinjerto",
@@ -138,7 +138,7 @@ const productosJson = {
             "img": "./img/img_prod/OATS para autoinjerto.webp",
             "desc": "Los sets de tubos para corte de donantes y receptores desechables del sistema de transferencia para autoinjerto osteocondral (OATS) facilitan la obtención de cilindros de cartílago osteocondral/hialino de diversos diámetros de un sitio donante superior y lateral a la muesca o por encima del surco terminal. Se crea un orificio receptor, de la profundidad adecuada, en el defecto condral para aceptar el injerto donante. El cilindro de hueso se inserta con un sistema de provisión de pins centradores para fijación de ajuste por presión. La obtención y la profundidad de inserción se controlan con marcas de 5 mm de profundidad. La colocación final del cilindro cosechado, al ras con el cartílago articular circundante, se logra con un medidor/nivelador de diámetro apenas más grande.",
             "descMob": "https://www.arthrex.com/es/rodilla/meniscal-cinch-ii-device",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "RODILLA ANCLAS CON SUTURA Corkscrew®️ FT",
@@ -148,7 +148,7 @@ const productosJson = {
             "img": "./img/img_prod/Corkscrew FT.webp",
             "desc": "La familia de anclas con sutura Corkscrew completamente roscadas fue diseñada para lograr el máximo de resistencia de fijación e inserción sencilla. El mecanismo interno de impulsión se conjuga con un original ojal de sutura FiberWire®️ para permitir suturas continuas a lo largo del ancla. Este diseño permite que el ancla se inserte al ras de la superficie del hueso cortical proporcionando una excelente resistencia de fijación y estabilidad al mismo que tiempo que impide el efecto “de retroceso” que puede ocurrir en anclajes convencionales con ojales sobresalientes. La configuración del insertador interno permite un elevado torque de inserción y el ojal de sutura hace innecesaria la orientación específica del ojal en el borde del tejido. Las anclas tienen una carga doble o triple de sutura FiberWire para lograr la mejor combinación posible y obtener la mayor fuerza de reparación.",
             "descMob": "https://www.arthrex.com/es/rodilla/meniscal-cinch-ii-device",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "SwiveLock®️",
@@ -158,7 +158,7 @@ const productosJson = {
             "img": "./img/img_prod/SwiveLock Rodilla.webp",
             "desc": "La línea de productos SwiveLock consiste en anclas sin nudos totalmente roscadas diseñadas para usarse con FiberWire, FiberTape, e injertos de tejido blando en técnicas de reparación y reconstrucción. La tensión es visualizada, ajustada y trabada en posición con el cuerpo del ancla SwiveLock. Las anclas SwiveLock están canuladas y tienen conducto de comunicación para minimizar el material. Las anclas están disponibles en materiales bioabsorbibles BioComposite o PLLA y no absorbibles PEEK o titanio.",
             "descMob": "https://www.arthrex.com/es/rodilla/meniscal-cinch-ii-device",
-            "categoria": "Cardio"
+            "categoria": "Trauma"
         },
         {
             "titulo": "NEUROESTIMULACION PROCLAIM",
@@ -168,7 +168,7 @@ const productosJson = {
             "img": "./img/img_prod/Proclaim.jpg",
             "desc": "Neuroestimulador, de corriente constante, único en el mercado con capacidad de ofrecer ondas alternativas de estimulación tónica y con ráfagas, de batería no recargable, con 24 programas de estimulación, con sistema Epiducer (para implante de electrodos por punción) o laminectomía (modelo PENTA de 5 columnas y 20 contactos), programador de mano para pacientes”.",
             "descMob": "https://corpomedica.com.ar/product/49/Proclaim.html",
-            "categoria": "Cardio"
+            "categoria": "Neuro"
         },
         {
             "titulo": "INFINITY",
@@ -186,7 +186,7 @@ const productosJson = {
             "- Plataforma programable con dispositivos digitales móbiles Apple via Bluetooth"+
             "- Sistema de fijación de catéter cerebral profundo Guardian",
             "descMob": "https://corpomedica.com.ar/product/4/Infinity.html",
-            "categoria": "Cardio"
+            "categoria": "Neuro"
         },
         {
             "titulo": "PRODIGY MRI",
@@ -196,7 +196,7 @@ const productosJson = {
             "img": "./img/img_prod/Prodigy MRI.jpg",
             "desc": "Neuroestimulador, de corriente constante, único en el mercado con capacidad de ofrecer ondas alternativas de estimulación tónica y con ráfagas, de batería recargable, con 24 programas de estimulación",
             "descMob": "https://corpomedica.com.ar/product/2/Prodigy-MRI.html",
-            "categoria": "Cardio"
+            "categoria": "Neuro"
         },
         {
             "titulo": "Nit-Occlud® PDA-R",
